ede8e6390b143d6ded46b1bad378d6ec9406415b
MichaelKonobeev <konobeev.michael@gmail.com>
2022-03-02 13:50:24 +0100

    Remove nonexisting tensorflow cpu extra feature

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "numpy>=1.16.4",
         "scipy>=1.5.0",
         "tensorboard>=1.15",
-        "tensorflow[cpu]>=2.8.0",
+        "tensorflow>=2.8.0",
         "torch>=1.5.1",
         "tqdm",
     ],
