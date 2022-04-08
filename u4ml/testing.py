b146ef4bc5bc43994038e8c60e7bb068e0e6a12a
MichaelKonobeev <konobeev.michael@gmail.com>
2022-04-08 15:48:56 +0200

    Add assertAllEqual to TorchTestCase

--- u4ml/testing.py
+++ u4ml/testing.py
@@ -21,6 +21,10 @@ class TorchTestCase(TestCase):
     random.seed(0)
     np.random.seed(0)
 
+  def assertAllEqual(self, actual, expected):
+    """ Checks that all elements of actual are equal to expected. """
+    nt.assert_array_equal(_np(actual), _np(expected))
+
   # pylint: disable=invalid-name
   def assertAllClose(self, actual, expected, rtol=1e-7, atol=0.):
     """ Checks that actual and expected arrays or torch tensors are equal. """
